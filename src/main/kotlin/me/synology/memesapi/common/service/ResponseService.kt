562d54ef6481452091d87f9fcc524aed069041b2
GHGHGHKO <gudrb963@gmail.com>
2022-12-12 22:47:50 +0900

    feat: request Validation check error response body

--- src/main/kotlin/me/synology/memesapi/common/service/ResponseService.kt
+++ src/main/kotlin/me/synology/memesapi/common/service/ResponseService.kt
@@ -2,6 +2,7 @@ package me.synology.memesapi.common.service
 
 import me.synology.memesapi.common.model.response.CommonResult
 import me.synology.memesapi.common.model.response.SingleResult
+import me.synology.memesapi.common.model.response.ValidationCheckResult
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,6 +14,14 @@ class ResponseService {
 
     fun failResult(code: Int, message: String): CommonResult {
         return CommonResult(false, code, message)
+    }
+
+    fun argumentsFailResults(
+        code: Int,
+        message: String,
+        allErrors: MutableList<ValidationCheckResult.FieldErrors>
+    ): ValidationCheckResult {
+        return ValidationCheckResult(false, code, message, allErrors)
     }
 
     fun <T> singleResult(data: T): SingleResult<T> {
